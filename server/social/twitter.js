0894521bccb677681f809f4839bb9a052e435d3e
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2018-11-05 22:56:38 -0300

    Verifica retorno da api

--- server/social/twitter.js
+++ server/social/twitter.js
@@ -6,17 +6,20 @@ var T = Twit(keys);
 
 exports.search = function (query) {
   return Q.Promise(function (resolve, reject) {
-    T.get('search/tweets', { q: '"' + query + '" -filter:retweets since:2018-07-10', count: 15, lang: 'pt', result_type: 'recent' }, function(err, data, response) {
+    T.get('search/tweets', { q: '"' + query + '" -filter:retweets since:2018-11-01', count: 15, lang: 'pt', result_type: 'recent' }, function(err, data, response) {
       var posts = [];
+
+      if (data.statuses && data.statuses.length > 0) {
+        posts = data.statuses.map(function (post) {
+          return { 
+            text: post.text,
+            id: post.id_str,
+            username: post.user.screen_name,
+            name: post.user.name
+          };
+        });
+      }
       
-      posts = data.statuses.map(function (post) {
-        return { 
-          text: post.text,
-          id: post.id_str,
-          username: post.user.screen_name,
-          name: post.user.name
-        };
-      });
       return resolve(posts);
     });
   });
